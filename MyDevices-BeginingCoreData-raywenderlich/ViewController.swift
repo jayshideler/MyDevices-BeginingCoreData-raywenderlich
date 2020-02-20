0d4d3b0448d9540ec65bf6e90068ecbf774f03b0
jayshideler <jayshideler@yahoo.com>
2020-02-20 11:19:57 -0700

    Initial Commit

---
+++ MyDevices-BeginingCoreData-raywenderlich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyDevices-BeginingCoreData-raywenderlich
+//
+//  Created by Jay Randall Shideler on 2/20/20.
+//  Copyright © 2020 Jay Randall Shideler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
